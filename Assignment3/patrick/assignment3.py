dd510d93c2b30616c6e17222560833abcf1a6ab3
Patrick <patrick.attimont@grenoble-inp.org>
2024-02-01 20:12:25 +0100

    problem 3

--- Assignment3/patrick/assignment3.py
+++ Assignment3/patrick/assignment3.py
@@ -4,8 +4,6 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import BernoulliNB
 
-tars = ["Assignment3/20021010_easy_ham.tar.bz2", "Assignment3/20021010_spam.tar.bz2"]
-
 encodings = ["utf-8", "ascii", "ISO-8859-1"]
 
 SEED = 1234
@@ -18,30 +16,45 @@ def decodeEmail(email):
         except UnicodeDecodeError:
             pass
 
-emails = []
-emails_category = []
+def getEmails(tar):
+    emails = []
 
-for tar in tars:
     files = tarfile.open(tar)
     for member in files.getmembers():
         file = files.extractfile(member)
         if file is not None:
             emails.append(decodeEmail(file))
-            if "spam" in tar:
-                emails_category.append(1)
-            else:
-                emails_category.append(0)
     files.close()
+    return emails
 
-x_train, x_test, y_train, y_test = train_test_split(emails, emails_category, test_size=0.25, random_state=SEED)
+def evaluate(emails, categories):
+    x_train, x_test, y_train, y_test = train_test_split(emails, categories, test_size=0.25, random_state=SEED)
 
-cv = CountVectorizer()
-x_train = cv.fit_transform(x_train)
-x_test = cv.transform(x_test)
+    cv = CountVectorizer()
+    x_train = cv.fit_transform(x_train)
+    x_test = cv.transform(x_test)
 
-bnb = MultinomialNB()
-bnb.fit(x_train, y_train)
+    mnb = MultinomialNB()
+    mnb.fit(x_train, y_train)
 
-category_predicted = bnb.predict(x_test)
+    bnb = BernoulliNB()
+    bnb.fit(x_train, y_train)
 
-print("Accuracy : ", (y_test == category_predicted).sum() / len(y_test))
+    category_predicted = bnb.predict(x_test)
+
+    print("Accuracy : ", (y_test == category_predicted).sum() / len(y_test))
+
+
+
+easy_ham = getEmails("Assignment3/20021010_easy_ham.tar.bz2")
+hard_ham = getEmails("Assignment3/20021010_hard_ham.tar.bz2")
+spam = getEmails("Assignment3/20021010_spam.tar.bz2")
+
+easy_and_spam = easy_ham + spam
+easy_and_spam_cat = [0] * len(easy_ham) + [1] * len(spam)
+
+hard_and_spam = hard_ham + spam
+hard_and_spam_cat = [0] * len(hard_ham) + [1] * len(spam)
+
+evaluate(easy_and_spam, easy_and_spam_cat)
+evaluate(hard_and_spam, hard_and_spam_cat)
