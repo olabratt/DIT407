5bba65de48c6b48da7404e207135b4b1d854d98e
Patrick <patrick.attimont@grenoble-inp.org>
2024-02-25 23:43:52 +0100

    Problem 3 wip

--- Assignment6/patrick/assignment6.py
+++ Assignment6/patrick/assignment6.py
@@ -1,21 +1,96 @@
+import torch
+from torch import nn
+from torch import optim
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 
-batch_size = 64
+batch_size = 32
+img_size = 28
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
 
 # Problem 1
 
 train_data = MNIST("Assignment6/patrick", train=True, download=True, transform=ToTensor())
 test_data = MNIST("Assignment6/patrick", train=False, download=True, transform=ToTensor())
-train_dataloader = DataLoader(train_data, batch_size=batch_size)
-test_dataloader = DataLoader(test_data, batch_size=batch_size)
+train_dataloader = DataLoader(train_data, batch_size=batch_size, num_workers=1, pin_memory=True)
+test_dataloader = DataLoader(test_data, batch_size=batch_size, num_workers=1, pin_memory=True)
 
-plt.imshow(train_data[0][0][0], cmap='Greys')
-plt.show()
-plt.imshow(test_data[0][0][0], cmap='Greys')
-plt.show()
+# print(train_data[0])
+# plt.imshow(train_data[0][0][0], cmap='Greys') # 5
+# plt.show()
+# plt.imshow(test_data[0][0][0], cmap='Greys')  # 7
+# plt.show()
 
 # Problem 2
 
+def accuracy(network, dataloader):
+  network.eval()
+  
+  total_correct = 0
+  total_instances = 0
+
+  with torch.no_grad():
+    for images, labels in dataloader:
+      images, labels = images.to(device), labels.to(device)
+
+      classifications = torch.argmax(network(images), dim=1)
+
+      correct_predictions = sum(classifications==labels).item()
+
+      total_correct+=correct_predictions
+      total_instances+=len(images)
+  return round(total_correct/total_instances, 3)
+
+def train(model, dataloader, loss_fn, optimizer):
+    for data, target in dataloader:
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        target_pred = model(data)
+
+        loss = loss_fn(target_pred, target)
+        loss.backward()
+        optimizer.step()
+
+NUM_EPOCHS = 10
+
+model = nn.Sequential(
+    nn.Flatten(),
+    nn.Linear(img_size * img_size, 300),
+    nn.ReLU(),
+    nn.Linear(300, 10),
+    nn.Sigmoid()
+).to(device)
+
+loss_fn = nn.CrossEntropyLoss().cuda()
+optimizer = optim.SGD(model.parameters(), lr = 0.01)
+
+# for i in range(NUM_EPOCHS):
+#     train(model, train_dataloader, loss_fn, optimizer)
+#     print(accuracy(model, test_dataloader))
+
+# Problem 3
+NUM_EPOCHS = 40
+
+model = nn.Sequential(
+    nn.Flatten(),
+    nn.Linear(img_size * img_size, 500),
+    nn.ReLU(),
+    nn.Linear(500, 300),
+    nn.Linear(300, 10),
+    nn.Sigmoid()
+).to(device)
+
+loss_fn = nn.CrossEntropyLoss().cuda()
+optimizer = optim.SGD(model.parameters(), lr = 0.01)
+
+for i in range(NUM_EPOCHS):
+    train(model, train_dataloader, loss_fn, optimizer)
+    print(accuracy(model, test_dataloader))
